f2b9c6241f171efba669e4a8f114d44d8370a1df
Lucas-Maurice Stein <lms@koempf24.de>
2026-05-05 12:45:23 +0200

    fix open graph scraping

---
+++ app/robots.ts
@@ -0,0 +1,22 @@
+// app/robots.ts
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "facebookexternalhit",
+                allow: "/",
+            },
+            {
+                userAgent: "Facebot",
+                allow: "/",
+            },
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+        ],
+        sitemap: "https://aulssp.de/sitemap.xml",
+    };
+}
