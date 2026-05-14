aae5f36861f3cf07e55a6cb1b7c4f312a9a4803f
Lucas-Maurice Stein <lms@koempf24.de>
2026-05-14 14:35:56 +0200

    hopefully fixed picute upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "5mb",
+        },
+    },
 };
 
 export default nextConfig;
